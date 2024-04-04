5f0b8fffb50b925cea3667599d1fbcaf98b82a68
nox-cyber0 <mhabubakar999@outlook.com>
2024-04-04 22:29:56 +0000

    Type error fix

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ interface Product {
 }
 
 export default function Home() {
-	const [selected, setSelected] = useState<Product | null>(null);
+	const [selected, setSelected] = useState<Product>('');
 
 	// console.log(selected); // Optional chaining to avoid errors
 
